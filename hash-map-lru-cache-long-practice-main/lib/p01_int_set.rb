0953d4ad839514c20e9c36915e0bdbac7560c725
Sanjid <dsanjid@gmail.com>
2023-07-24 16:57:50 -0400

    resizingintset

--- hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
+++ hash-map-lru-cache-long-practice-main/lib/p01_int_set.rb
@@ -1,14 +1,29 @@
 class MaxIntSet
+
+  attr_reader :store
   def initialize(max)
+    @store = Array.new(max) {false}
   end
 
   def insert(num)
+    if num <= 50 && num >= 0
+      @store[num] = true
+    else 
+      raise 'Out of bounds'
+    end
+    
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    if @store[num] == true 
+      return true 
+    else
+      return false
+    end
   end
 
   private
@@ -21,17 +36,26 @@ class MaxIntSet
 end
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    @store[num] = true
+
   end
 
   def remove(num)
+    @store[num] = false
   end
 
   def include?(num)
+    if @store[num] == true 
+      return true 
+    else 
+      return false
+    end
   end
 
   private
@@ -46,7 +70,7 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
@@ -54,12 +78,17 @@ class ResizingIntSet
   end
 
   def insert(num)
+    if !@store[num_buckets % 20].include?(num)
+      @store[num_buckets % 20] << num
+    end
   end
 
   def remove(num)
   end
 
   def include?(num)
+    return false unless @store[num] == true 
+    return true
   end
 
   private
